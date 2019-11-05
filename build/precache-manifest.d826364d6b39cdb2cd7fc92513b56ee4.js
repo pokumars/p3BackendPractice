bc6384d5cab5e512a98b1cc447749fafb66e246b
oheneba <ohenebakob@hotmail.com>
2019-11-05 09:55:58 +0200

    removed gitignore from build dir

---
+++ build/precache-manifest.d826364d6b39cdb2cd7fc92513b56ee4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94837e249c04dad9dfec963d293e4aa9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e7f8458517d56a3e9df",
+    "url": "/static/css/main.2245a4f6.chunk.css"
+  },
+  {
+    "revision": "1af2e68c30103fa82895",
+    "url": "/static/js/2.f3384baa.chunk.js"
+  },
+  {
+    "revision": "6e7f8458517d56a3e9df",
+    "url": "/static/js/main.9d4cdc44.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
